ce847e2374da83bb94654a4dcfe6cb1a169fa4ce
camelia0404 <baidicamelia0@gmail.com>
2024-11-02 17:34:33 +0100

    Update header-plateau.h

--- header-plateau.h
+++ header-plateau.h
@@ -1,3 +1,3 @@
 //Contient les données du plateau (un tableau pour le quadrillage qui évolue en fonction des actions)
-int largeur_plateau (); // Retourne la largeur du plateau de jeu.
-int longueur_plateau (); // Retourne la longueur du plateau de jeu.
+int largeur_plateau; // Variable qui représente la largeur du plateau de jeu. 
+int longueur_plateau; // Variable qui représente la longueur du plateau de jeu.
