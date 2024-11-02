4e563a27a31c6a79662a9db771b489c4f8a2ecac
camelia0404 <baidicamelia0@gmail.com>
2024-11-02 17:39:24 +0100

    Update header-joueur.h

--- header-joueur.h
+++ header-joueur.h
@@ -1,2 +1,2 @@
 // Header pour les joueurs, il contient les données relative aux joueurs.
-int couleur(); // Permet de choisir la couleur du pion.
+int couleur; // Variable qui représente la couleur du pion.
