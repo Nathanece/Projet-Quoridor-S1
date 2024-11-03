0e87b94c592334557ea6b750b8283a77e4cc3cf9
Nathanece <nathan.ledesma@edu.ece.fr>
2024-11-03 19:30:24 +0100

    Commentaire sur de possibles sous-programmes dans plateau.c

---
+++ code-plateau.c
@@ -0,0 +1,7 @@
+//Contient les sous-programmes qui modélisent l'évolution de la partie (poser barriere, déplacer pion) sur le plateau
+
+void deplacement_pion();
+//Sous-programme qui execute le déplacement d'un pion à la suite d'une action choisie. Il sera surement appelé par la fonction du menu
+
+void placer_barriere();
+//Sous-programme qui execute le placement d'une barrière. 
--- code-plateau.cpp
+++
@@ -1,1 +0,0 @@
-//Contient les sous-programmes qui modélisent l'évolution de la partie (poser barriere, déplacer pion) sur le plateau
