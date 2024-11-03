5197ee4aed991e2438fb3ce32e48678aa7c99153
Nathanece <nathan.ledesma@edu.ece.fr>
2024-11-03 19:26:56 +0100

    Commentaire sur de possibles sous-programmes dans le menu.c

--- code-menu.cpp
+++ code-menu.cpp
@@ -1,1 +1,8 @@
 //Contient les sous-programmes permettant le choix des actions par le joueur
+
+void menu_initialisation(); 
+//Ce sous-programme servira à initialiser la partie (nb joueurs, couleur des joueurs, ...)
+
+void menu_actions();
+/*Ce sous-programme permettra de choisir l'action à executer (déplacement ou placer barrière). Nous pourrions mettre un timer
+pour que le joueur soit forcé de choisir une action (sinon son tour est sauté). */
